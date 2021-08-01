18178b45db9fa94afc215105e434fdc717838c08
demiurgen <duen@mail.ru>
2021-08-01 20:15:47 +0300

    Окончательно задвинул модификаторы с хоум роу по сторонам

--- keyboards/sofle/keymaps/via/keymap.c
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -74,8 +74,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_HOME] = LAYOUT(
     KC_GRV,         KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                    KC_CIRC, KC_AMPR,     KC_ASTR, KC_RBRC, KC_MINS,          KC_EQL,
-    C_S_T(KC_TAB),  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,        KC_I,    KC_O,    C_S_T(KC_P),      KC_LBRC,
-    LCTL_T(KC_ESC), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,        KC_K,     KC_L,    RCTL_T(KC_SCLN),  KC_QUOT,
+    C_S_T(KC_TAB),  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,             C_S_T(KC_LBRC),
+    LCTL_T(KC_ESC), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,        KC_K,     KC_L,   KC_SCLN,          RCTL_T(KC_QUOT),
     OSM(MOD_LSFT),  LCA_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,     C(KC_X),                KC_MPLY,  KC_N,    KC_M,        KC_COMM, KC_DOT,  LCA_T(KC_SLSH),   OSM(MOD_LSFT),
                           KC_LGUI, KC_TRNS, LALT_T(KC_TRNS), MO(_NUM), LSFT_T(KC_SPC),     LT(_NUM, KC_SPC), LT(_NAV,KC_BSPC), RALT_T(KC_TRNS), KC_ENT,  KC_BSLS),
 
