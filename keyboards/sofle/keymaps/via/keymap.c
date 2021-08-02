95b7f6610b7e8734e5580acdb7d9ca0bda1b610c
demiurgen <duen@mail.ru>
2021-08-02 14:08:00 +0300

    Мелкие правки раскладки

--- keyboards/sofle/keymaps/via/keymap.c
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -29,7 +29,7 @@ enum layer_names {
 enum custom_keycodes {
     M_ARROW = SAFE_RANGE,
     M_SLCT,
-    M_MACRO1,
+    M_AF4,
     M_MACRO2,
     M_MACRO3,
     M_MACRO4,
@@ -64,6 +64,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         //     return 200;
         // case TD(TD_MCR2):
         //     return 200;
+        case OSM(MOD_LSFT):
+            return 200;
         case TD(TD_SCLN):
             return 200;
         default:
@@ -90,6 +92,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // when keycode QMKURL is released
         }
         break;
+    case M_AF4:
+        if (record->event.pressed) {
+            tap_code16(A(KC_F4));
+            // SEND_STRING(SS_DOWN(X_LALT)
+            //             SS_TAP(X_F4)
+            //             SS_UP(X_LALT));
+        } else {
+            // when keycode QMKURL is released
+        }
+        break;
 
     // case MY_OTHER_MACRO:
     //     if (record->event.pressed) {
@@ -102,39 +114,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_HOME] = LAYOUT(
-    KC_GRV,         KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                              KC_CIRC, KC_AMPR, KC_ASTR, KC_RBRC,KC_MINS,       KC_EQL,
-    C_S_T(KC_TAB),  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,          C_S_T(KC_LBRC),
-    LCTL_T(KC_ESC), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,    KC_J,    KC_K,    KC_L,   TD(TD_SCLN),   RCTL_T(KC_QUOT),
-    OSM(MOD_LSFT),  LCA_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,     C(KC_X),         KC_MPLY,   KC_N,    KC_M,    KC_COMM, KC_DOT, LCA_T(KC_SLSH),OSM(MOD_LSFT),
-                          KC_LGUI, KC_TRNS, LALT_T(KC_NO),   MO(_NUM), LSFT_T(KC_SPC),  LT(_NUM, KC_SPC), LT(_NAV,KC_BSPC), RALT_T(KC_NO), KC_ENT, KC_BSLS),
+    LT(_FN4, KC_GRV),KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                              KC_CIRC, KC_AMPR, KC_ASTR, KC_RBRC,KC_MINS,       KC_EQL,
+    C_S_T(KC_TAB),   KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,          C_S_T(KC_LBRC),
+    LCTL_T(KC_ESC),  KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,    KC_J,    KC_K,    KC_L,   TD(TD_SCLN),   RCTL_T(KC_QUOT),
+    OSM(MOD_LSFT),   LCA_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,     C(KC_X),         KC_MPLY,   KC_N,    KC_M,    KC_COMM, KC_DOT, LCA_T(KC_SLSH),OSM(MOD_LSFT),
+                          KC_LGUI, KC_TRNS, LALT_T(KC_NO),   MO(_NUM), LSFT_T(KC_SPC),  LT(_NUM, KC_SPC), LT(_NAV,KC_BSPC), RALT_T(KC_NO), LT(_FN4, KC_ENT), KC_BSLS),
 
 	[_GAME] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                            DM_PLY1,  DM_PLY2, DM_REC1, DM_REC2, KC_LOCK, DM_RSTP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                            KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,      KC_G,                            KC_H,     KC_J,    KC_K,    KC_L,    TD(TD_SCLN),    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_MUTE,      TG(_GAME), KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
-                      KC_F9, KC_BSPC, KC_LALT,   MO(_FN4), KC_SPC,         KC_SPC,  LT(_NAV,KC_BSPC), KC_RCTL, EH_LEFT, EH_RGHT),
+                      KC_F9, KC_BSPC, KC_LALT,   MO(_FN4), KC_SPC,         KC_ENT,  LT(_NAV,KC_BSPC), KC_RCTL, EH_LEFT, EH_RGHT),
 
 	[_NUM] = LAYOUT(
-    KC_TRNS, KC_F1,   KC_F2,      KC_F3,   KC_F4,       DM_REC1,                            KC_F6,   KC_F7, KC_F8,   KC_F9,  KC_F10,  KC_F11,
-    KC_CAPS, KC_TRNS, M_ARROW,    KC_LBRC, KC_RBRC,     DM_RSTP,                            KC_PCMM, KC_P7, KC_P8,   KC_P9,  KC_PAST, KC_F12,
-    KC_TRNS, KC_LCBR, KC_RCBR,    KC_LPRN, KC_RPRN,     DM_PLY1,                            KC_PDOT, KC_P4, KC_P5,   KC_P6,  KC_PMNS, KC_PPLS,
-    KC_TRNS, KC_TRNS, A(KC_X),    A(KC_C), A( KC_V),    A(KC_B), KC_SLEP,         TG(_GAME), KC_TRNS, KC_P1, KC_P2,   KC_P3,  KC_PSLS, KC_PENT,
-                      KC_LOCK,    KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,        KC_TRNS,   KC_P0,   KC_P0, KC_TRNS, KC_TRNS),
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                              KC_F6,   KC_F7, KC_F8,   KC_F9,  KC_F10,  KC_F11,
+    KC_CAPS, M_ARROW, KC_GT,   KC_RBRC, KC_RPRN,  KC_RCBR,                            KC_PCMM, KC_P7, KC_P8,   KC_P9,  KC_PAST, KC_F12,
+    DM_RSTP, M_SLCT,  KC_LT,   KC_LBRC, KC_LPRN,  KC_LCBR,                            KC_PDOT, KC_P4, KC_P5,   KC_P6,  KC_PMNS, KC_PPLS,
+    DM_REC1, DM_PLY1, A(KC_X), A(KC_C), A( KC_V), A(KC_B), KC_SLEP,         TG(_GAME),KC_TRNS, KC_P1, KC_P2,   KC_P3,  KC_PSLS, KC_PENT,
+                      KC_LOCK, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,         KC_TRNS,  KC_P0,   KC_P0, KC_TRNS, KC_TRNS),
 
 	[_NAV] = LAYOUT(
-    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             G(KC_1),    G(KC_2),  G(KC_3), G(KC_4),    G(KC_5), G(KC_6),
-    KC_TRNS, KC_TRNS,    M_SLCT,  KC_MS_U, KC_BTN2, KC_TRNS,                             KC_DEL,     KC_HOME,  KC_END,  KC_TRNS,    KC_INS,  KC_PGUP,
-    KC_TRNS, KC_BTN2,    KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                             KC_LEFT,    KC_DOWN,  KC_UP,   KC_RGHT,    KC_ENT,  KC_PGDN,
-    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PWR,            KC_TRNS, C(KC_LEFT), KC_TRNS,  KC_TRNS, C(KC_RGHT), KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_ACL0, KC_TRNS,           KC_NO,   KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             G(KC_1),    G(KC_2),  G(KC_3), G(KC_4),    G(KC_5), G(KC_6),
+    KC_TRNS, KC_PSCR, KC_BTN1, KC_MS_U, KC_TRNS, KC_TRNS,                             KC_DEL,     KC_HOME,  KC_END,  KC_TRNS,    KC_INS,  KC_PGUP,
+    KC_TRNS, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                             KC_LEFT,    KC_DOWN,  KC_UP,   KC_RGHT,    KC_ENT,  KC_PGDN,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PWR,            KC_TRNS, C(KC_LEFT), KC_TRNS,  KC_TRNS, C(KC_RGHT), KC_TRNS, M_AF4,
+                      KC_TRNS, KC_TRNS, KC_ACL0, KC_ACL0, KC_ACL0,           KC_NO,   KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS),
 
 	[_FN4] = LAYOUT(
     KC_NO,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
     RESET,    KC_NO, DF(0),   DF(1),   CG_TOGG, TG(_FN4),                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-    TG(_FN4), KC_NO, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,                           KC_NO,   KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
-    KC_NO,    KC_NO, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        TG(_FN4), KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,
-                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    TG(_FN4), KC_NO, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,                           KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,
+    KC_NO,    KC_NO, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        TG(_FN4), KC_NO,   KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
+                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, MO(_FN4), KC_TRNS)
 };
 
 
@@ -151,9 +163,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 // --------------- NAV------------------
     } else if (IS_LAYER_ON(_NAV)) {
             if (clockwise) {
-                tap_code16(DM_RSTP);
+                tap_code16(KC_VOLU);
             } else {
-                tap_code16(DM_REC1);
+                tap_code16(KC_VOLD);
             }
 // --------------- NUM------------------
     } else if (IS_LAYER_ON(_NUM)) {
